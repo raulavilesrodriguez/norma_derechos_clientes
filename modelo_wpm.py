7abb1f8b6cd135a47389bbd1b1832316bc047083
Raul Aviles <finbravilesr@gmail.com>
2022-06-02 15:33:29 -0500

    modelo modificado

--- modelo_wpm.py
+++ modelo_wpm.py
@@ -6,30 +6,27 @@ import matplotlib.pyplot as plt
 def main():
     # Matriz de alternativas
     matrix = [
-        [11147, 3, 1480, 4, 2],  # alternativa 1: ModificaciónNormativa
-        [14332, 2, 2400, 2, 1],  # alternativa 2: ActividadesControl
-        [14332, 4, 840, 2, 1], # alternativa 3: Difusión
-        [15924, 1, 1200, 1, 1], # alternativa 4: No Acción
+        [383052, 17760, 523274, 150000],  # alternativa 1: ModificaciónNormativa
+        [116093, 24480, 790233, 0.1],  # alternativa 2: Difusión y ActividadesControl
+        [0, 14400, 906326, 0.1], # alternativa 3: No Acción
     ]
 
-    objectives = [min, max, min, max, min]
+    objectives = [max, min, min, min]
 
     dm = skc.mkdm(
         matrix,
         objectives,
-        weights=[0.30, 0.20, 0.10, 0.30, 0.10],
+        weights=[0.25, 0.25, 0.25, 0.25],
         alternatives=[
-            'ModificaciónNormativa', 
-            'ActividadesControl', 
-            'Difusión', 
+            'ModNormativa', 
+            'Comunicación',  
             'NoAcción'
         ],
         criteria=[
-            'NumReclamos',
-            'ImagenInstitucional',
+            'BenefCliente',
             'CostoArcotel',
-            'Transparencia',
-            'CostoPrestadores'
+            'CostoCliente',
+            'CostoPrestador'
         ],
     )
 
@@ -63,6 +60,7 @@ def main():
     # Se ajusta el layout al contenido
     fig.tight_layout()
     plt.show()
+
 
 if __name__ == "__main__":
     main()
